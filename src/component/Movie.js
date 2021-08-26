78ff3b5e92a5c09dbb1ccc620dfc45b3df440d5f
younyikim <rlakuku1221@gmail.com>
2021-08-26 20:38:38 +0900

    movie app : ui 수정

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -5,7 +5,7 @@ import "./Movie.css"
 
 function Movie({ id, year, title, summary, poster, genres }) {
     return (
-        <Link to={{
+        <Link className="movie__detailLink" to={{
             pathname: `/movie/${id}`,
             state: {
                 year, title, summary, poster, genres
@@ -18,7 +18,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
                     <h5 className="movie__year">{year}</h5>
                     <ul className="movie__genres">
                         {genres.map((genre, index) =>
-                            <li key={index} className="genres__genre">{index}{genre}</li>)}
+                            <li key={index} className="genres__genre">{genre}</li>)}
                     </ul>
                     <p className="movie__summary">{summary.slice(0, 140)}...</p>
                 </div>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,30 +1,40 @@
 import React from 'react';
+import './Detail.css';
 
 class Detail extends React.Component {
     componentDidMount() {
         const { location, history } = this.props;
-        console.log(location.state);
-        // 여기서 history의 go, goback, gofoward 등이 url을 변경한다.
-        console.log(this.props);
-        // 카드를 클릭하지 않고, url에 바로 /movie-detail을 치면 state 가 undefined이다. 이러한 경우, home으로 redirect
+
         if (location.state === undefined) {
             history.push("/");
         }
     }
+
     render() {
         const { location } = this.props;
-
-        // render 실행 후, componetnDidMount가 실행되기 때문에, url을 한번더 엔터를 누르면 에러가 발생한다.
-        // 이를 방지 하기 위해 location이 존재하는 지 체크하고, 없는 경우 null을 리턴하고 다시 home 으로 돌아간다.
+        const genres = location.state.genres;
 
         if (location.state) {
             return (
-                <span>{location.state.title}</span>
+                <div className="detail">
+                    <div className="detail__data">
+                        <img src={location.state.poster} alt={location.state.title} title={location.statetitle} />
+                        <h3 className="detail__title">{location.state.title}</h3>
+                        <h5 className="detail_year">{location.state.year}</h5>
+                        <p className="detail_summary">{location.state.summary} ... </p>
+                        <ul className="detail__genres">
+                            {
+                                genres.map((genre, index) =>
+                                    <li key={index} className="detail__genre">{genre}</li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                </div>
             );
         } else {
             return null;
         }
-
     }
 }
 
